557af60aa4ea5cd3c8b92e5a5f41797a5e5eadf8
fateme Mirali <fatememirali1374@gmail.com>
2024-02-05 11:44:57 +0330

    create todo app with reduxtoolkit

---
+++ src/components/Todos/TodoItem.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import {
+  deleteTodo,
+  toggleTodo,
+} from "../features/todo/todoSlice";
+
+const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch();
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center gap-1">
+          <input
+            onChange={(e) =>
+              dispatch(toggleTodo({ id, completed: !completed }))
+            }
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+          ></input>
+          <span>{title}</span>
+        </span>
+        <button
+          onClick={() => dispatch(deleteTodo({ id }))}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
